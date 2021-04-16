b84d0c98c0874f9c20993123398367443ff4dc9d
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2021-04-16 19:19:03 -0300

    getClosestServerProtocol

--- src/main/kotlin/com/viaversion/aas/provider/AspirinVersionProvider.kt
+++ src/main/kotlin/com/viaversion/aas/provider/AspirinVersionProvider.kt
@@ -5,7 +5,7 @@ import us.myles.ViaVersion.api.data.UserConnection
 import us.myles.ViaVersion.protocols.base.BaseVersionProvider
 
 object AspirinVersionProvider : BaseVersionProvider() {
-    override fun getServerProtocol(connection: UserConnection): Int {
+    override fun getClosestServerProtocol(connection: UserConnection): Int {
         val ver = connection.channel!!.pipeline().get(MinecraftHandler::class.java).data.viaBackServerVer
         if (ver != null) return ver
         return super.getServerProtocol(connection)
