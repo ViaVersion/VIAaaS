29375aecd0b1398ce0c3c88ef1934dddbf20f414
creeper123123321 <creeper123123321@gmail.com>
2021-05-30 17:34:35 -0300

    try to make install work by caching

--- src/main/resources/web/sw.js
+++ src/main/resources/web/sw.js
@@ -6,6 +6,37 @@ self.addEventListener("notificationclick", event => {
   viac.postMessage({tag: event.notification.tag, action: event.action});
 });
 
-addEventListener("fetch", e => {
-  // chrome please show "add to home screen"
+// stolen from https://github.com/mozilla/serviceworker-cookbook/blob/master/strategy-network-or-cache/service-worker.js (MIT license)
+
+var CACHE = "network-or-cache";
+
+self.addEventListener("install", evt => {
+  evt.waitUntil(cache(["./"]));
 });
+
+self.addEventListener("fetch", evt => {
+  evt.respondWith(
+    fromNetwork(evt.request)
+      .catch(() => fromCache(evt.request))
+  );
+});
+
+addEventListener("message", e => {
+  if (e.data.action == "cache") {
+    event.waitUntil(cache(e.data.urls));
+  }
+});
+
+function cache(urls) {
+  return caches.open(CACHE).then(cache => cache.addAll(urls));
+}
+
+function fromNetwork(request) {
+  return fetch(request);
+}
+
+function fromCache(request) {
+  return caches.open(CACHE)
+    .then(cache => cache.match(request))
+    .then(matching => matching || Promise.reject("no-match"));
+}
