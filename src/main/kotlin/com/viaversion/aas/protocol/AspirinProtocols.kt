b0e09bee906de6951d3ea8ffd54df53f5de41588
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2021-06-06 10:37:31 -0300

    1.7.2 -> 1.7.6 was moved to viarewind

--- src/main/kotlin/com/viaversion/aas/protocol/AspirinProtocols.kt
+++ src/main/kotlin/com/viaversion/aas/protocol/AspirinProtocols.kt
@@ -1,17 +1,11 @@
 package com.viaversion.aas.protocol
 
 import com.viaversion.aas.protocol.id47toid5.Protocol1_8To1_7_6
-import com.viaversion.aas.protocol.id5toid4.Protocol1_7_6to1_7_2
 import com.viaversion.viaversion.api.Via
 import com.viaversion.viaversion.api.protocol.version.ProtocolVersion
 
 // cursed 1.7 -> 1.8 from https://github.com/Gerrygames/ClientViaVersion
 // + https://github.com/creeper123123321/ViaRewind/tree/17to18
 fun registerAspirinProtocols() {
-    Via.getManager().protocolManager.registerProtocol(
-        Protocol1_7_6to1_7_2,
-        ProtocolVersion.v1_7_6,
-        ProtocolVersion.v1_7_1
-    )
     Via.getManager().protocolManager.registerProtocol(Protocol1_8To1_7_6, ProtocolVersion.v1_8, ProtocolVersion.v1_7_6)
 }
--- src/main/kotlin/com/viaversion/aas/protocol/Util.kt
+++ src/main/kotlin/com/viaversion/aas/protocol/Util.kt
@@ -3,20 +3,8 @@ package com.viaversion.aas.protocol
 import com.viaversion.viaversion.api.minecraft.Position
 import com.viaversion.viaversion.api.protocol.packet.PacketWrapper
 import com.viaversion.viaversion.api.protocol.remapper.ValueReader
-import com.viaversion.viaversion.api.protocol.remapper.ValueTransformer
 import com.viaversion.viaversion.api.protocol.remapper.ValueWriter
 import com.viaversion.viaversion.api.type.Type
-
-val INSERT_DASHES: ValueTransformer<String, String> = object : ValueTransformer<String, String>(Type.STRING) {
-    override fun transform(packetWrapper: PacketWrapper, s: String?): String {
-        val builder = StringBuilder(s)
-        builder.insert(20, "-")
-        builder.insert(16, "-")
-        builder.insert(12, "-")
-        builder.insert(8, "-")
-        return builder.toString()
-    }
-}
 
 val xyzToPosition = ValueReader { packetWrapper: PacketWrapper ->
     val x = packetWrapper.read(Type.INT)
@@ -30,17 +18,17 @@ val xyzUBytePos = ValueReader { packetWrapper: PacketWrapper ->
     val z = packetWrapper.read(Type.INT)
     Position(x, y, z)
 }
-val xyzUBytePosWriter: ValueWriter<Position> = ValueWriter<Position> { packetWrapper: PacketWrapper, pos: Position ->
+val xyzUBytePosWriter = ValueWriter { packetWrapper: PacketWrapper, pos: Position ->
     packetWrapper.write(Type.INT, pos.x)
     packetWrapper.write(Type.UNSIGNED_BYTE, pos.y.toShort())
     packetWrapper.write(Type.INT, pos.z)
 }
-val xyzShortPosWriter: ValueWriter<Position> = ValueWriter<Position> { packetWrapper: PacketWrapper, pos: Position ->
+val xyzShortPosWriter = ValueWriter { packetWrapper: PacketWrapper, pos: Position ->
     packetWrapper.write(Type.INT, pos.x)
     packetWrapper.write(Type.SHORT, pos.y.toShort())
     packetWrapper.write(Type.INT, pos.z)
 }
-val xyzShortPos: ValueReader<Position> = ValueReader<Position> { packetWrapper: PacketWrapper ->
+val xyzShortPos = ValueReader { packetWrapper: PacketWrapper ->
     val x = packetWrapper.read(Type.INT)
     val y = packetWrapper.read(Type.SHORT)
     val z = packetWrapper.read(Type.INT)
