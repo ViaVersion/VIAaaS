68a8895eaebb6834f0ac7b820811dd0e5dae2963
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2021-07-06 12:46:17 -0300

    update via

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ compileKotlin.kotlinOptions.jvmTarget = "11"
 val gitVersion: groovy.lang.Closure<String> by extra
 
 group = "com.github.creeper123123321.viaaas"
-version = "0.4.6+" + try {
+version = "0.4.7+" + try {
     gitVersion()
 } catch (e: Exception) {
     "unknown"
@@ -59,8 +59,8 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
 
-    val vvVer = "4.0.1-1.17.1-rc2-SNAPSHOT"
-    val vbVer = "4.0.1-1.17.1-rc1-SNAPSHOT"
+    val vvVer = "4.0.1"
+    val vbVer = "4.0.1"
     val vrVer = "e3f7a96"
     implementation("com.viaversion:viaversion:$vvVer") { isTransitive = false }
     implementation("com.viaversion:viabackwards:$vbVer") { isTransitive = false }
