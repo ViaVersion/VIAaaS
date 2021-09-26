396dcb40249ed0ef292e223c32e7f130699e7153
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2021-09-26 10:01:24 -0300

    changed redirect url whitelist

--- src/main/resources/web/js/config.js
+++ src/main/resources/web/js/config.js
@@ -1,11 +1,9 @@
 // See https://docs.microsoft.com/en-us/azure/active-directory/develop/scenario-spa-app-registration
-// You'll need to add your URL to Azure as a Single Page Application
+// You'll need to add the page as a Redirect URL to Azure as a Single Page Application
 
 // Azure Client ID
 const azureClientId = "a370fff9-7648-4dbf-b96e-2b4f8d539ac2";
-
+// Origins that we'll not use https://viaversion.github.io/VIAaaS/ as redirect URL
 const whitelistedOrigin = [
-    "https://via-login.geyserconnect.net",
-    "https://via.re.yt.nom.br",
-    "https://viaaas.noxt.cf"
+    "https://via.re.yt.nom.br"
 ];
