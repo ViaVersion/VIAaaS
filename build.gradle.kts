92c52d525359272894f68ea8a240f22b160afd1b
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2021-10-21 19:06:22 -0300

    21w42a

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import java.nio.file.Files as JFiles
 
 buildscript {
     repositories { mavenCentral() }
-    dependencies { classpath("com.github.hazendaz:htmlcompressor:1.7.1") }
+    dependencies { classpath("com.github.hazendaz:htmlcompressor:1.7.2") }
 }
 
 plugins {
@@ -60,8 +60,8 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
 
-    val vvVer = "4.1.0-21w41a-SNAPSHOT"
-    val vbVer = "4.1.0-21w41a-SNAPSHOT"
+    val vvVer = "4.1.0-21w42a-SNAPSHOT"
+    val vbVer = "4.1.0-21w42a-SNAPSHOT"
     val vrVer = "f591a29"
     implementation("com.viaversion:viaversion:$vvVer") { isTransitive = false }
     implementation("com.viaversion:viabackwards:$vbVer") { isTransitive = false }
@@ -84,7 +84,7 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-iostreams:$log4jVer")
     implementation("org.apache.logging.log4j:log4j-jul:$log4jVer")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVer")
-    implementation("org.jline:jline-terminal-jansi:3.20.0")
+    implementation("org.jline:jline-terminal-jansi:3.21.0")
     implementation("org.slf4j:slf4j-api:$slf4jVer")
 
     val ktorVersion = "1.6.4"
--- src/main/kotlin/com/viaversion/aas/protocol/Util.kt
+++ src/main/kotlin/com/viaversion/aas/protocol/Util.kt
@@ -8,29 +8,29 @@ import com.viaversion.viaversion.api.type.Type
 
 val xyzToPosition = ValueReader { packetWrapper: PacketWrapper ->
     val x = packetWrapper.read(Type.INT)
-    val y = packetWrapper.read(Type.INT).toShort()
+    val y = packetWrapper.read(Type.INT)
     val z = packetWrapper.read(Type.INT)
     Position(x, y, z)
 }
 val xyzUBytePos = ValueReader { packetWrapper: PacketWrapper ->
     val x = packetWrapper.read(Type.INT)
-    val y = packetWrapper.read(Type.UNSIGNED_BYTE)
+    val y = packetWrapper.read(Type.UNSIGNED_BYTE).toInt()
     val z = packetWrapper.read(Type.INT)
     Position(x, y, z)
 }
 val xyzUBytePosWriter = ValueWriter { packetWrapper: PacketWrapper, pos: Position ->
-    packetWrapper.write(Type.INT, pos.x)
-    packetWrapper.write(Type.UNSIGNED_BYTE, pos.y.toShort())
-    packetWrapper.write(Type.INT, pos.z)
+    packetWrapper.write(Type.INT, pos.x())
+    packetWrapper.write(Type.UNSIGNED_BYTE, pos.y().toShort())
+    packetWrapper.write(Type.INT, pos.z())
 }
 val xyzShortPosWriter = ValueWriter { packetWrapper: PacketWrapper, pos: Position ->
-    packetWrapper.write(Type.INT, pos.x)
-    packetWrapper.write(Type.SHORT, pos.y.toShort())
-    packetWrapper.write(Type.INT, pos.z)
+    packetWrapper.write(Type.INT, pos.x())
+    packetWrapper.write(Type.SHORT, pos.y().toShort())
+    packetWrapper.write(Type.INT, pos.z())
 }
 val xyzShortPos = ValueReader { packetWrapper: PacketWrapper ->
     val x = packetWrapper.read(Type.INT)
-    val y = packetWrapper.read(Type.SHORT)
+    val y = packetWrapper.read(Type.SHORT).toInt()
     val z = packetWrapper.read(Type.INT)
     Position(x, y, z)
 }
