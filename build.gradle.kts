aea4f91c6ec4e1621c5f16480f50fde43749f05b
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2021-11-13 16:36:20 -0300

    update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation(kotlin("reflect"))
 
     val vvVer = "4.1.0-1.18-pre1-SNAPSHOT"
-    val vbVer = "4.1.0-21w44a-SNAPSHOT"
+    val vbVer = "4.1.0-1.18-pre1-SNAPSHOT"
     val vrVer = "d93606d"
     implementation("com.viaversion:viaversion:$vvVer") { isTransitive = false }
     implementation("com.viaversion:viabackwards:$vbVer") { isTransitive = false }
