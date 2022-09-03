3cbacd4f262eef0378640fad63cd26af37d9bf29
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2022-09-03 12:33:53 -0300

    use var for now

--- src/main/resources/web/js/config.js
+++ src/main/resources/web/js/config.js
@@ -1,5 +1,6 @@
 // See https://docs.microsoft.com/en-us/azure/active-directory/develop/scenario-spa-app-registration
 // You'll need to add the page as a Redirect URL to Azure as a Single Page Application
+// noinspection ES6ConvertVarToLetConst - TODO fix this
 
 // Azure Client ID
 const azureClientId = "a370fff9-7648-4dbf-b96e-2b4f8d539ac2";
@@ -8,6 +9,6 @@ const whitelistedOrigin = [
     "https://via.re.yt.nom.br"
 ];
 // Default CORS Proxy config
-const defaultCorsProxy = "https://cors.re.yt.nom.br/";
+var defaultCorsProxy = "https://cors.re.yt.nom.br/";
 // Default instance suffix, in format "viaaas.example.com[:25565]", null to use the page hostname;
-const defaultInstanceSuffix = null;
+var defaultInstanceSuffix = null;
