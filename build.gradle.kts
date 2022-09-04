986fbb5a28e04befa5a65cece8c956660044c730
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2022-09-04 13:02:30 -0300

    update viarewind

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
 
     val vvVer = "4.4.3-SNAPSHOT"
     val vbVer = "4.4.2"
-    val vrVer = "0476674"
+    val vrVer = "e6b8b69"
     implementation("com.viaversion:viaversion:$vvVer") { isTransitive = false }
     implementation("com.viaversion:viabackwards:$vbVer") { isTransitive = false }
     implementation("com.github.ViaVersion.ViaRewind:viarewind-all:$vrVer") { isTransitive = false }
