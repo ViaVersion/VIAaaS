dc759580558c4688f6574be23602f1d14c1259c1
EnZaXD <60033407+FlorianMichael@users.noreply.github.com>
2023-06-18 20:41:21 +0200

    Removed MovementTransmitterProvider use() call (#229)

--- src/main/java/com/viaversion/aas/platform/AspirinLoader.java
+++ src/main/java/com/viaversion/aas/platform/AspirinLoader.java
@@ -8,15 +8,12 @@ import com.viaversion.viaversion.api.Via;
 import com.viaversion.viaversion.api.platform.ViaPlatformLoader;
 import com.viaversion.viaversion.api.protocol.version.VersionProvider;
 import com.viaversion.viaversion.protocols.protocol1_9to1_8.providers.CompressionProvider;
-import com.viaversion.viaversion.protocols.protocol1_9to1_8.providers.MovementTransmitterProvider;
-import com.viaversion.viaversion.velocity.providers.VelocityMovementTransmitter;
 import net.raphimc.vialegacy.protocols.release.protocol1_7_2_5to1_6_4.providers.EncryptionProvider;
 import net.raphimc.vialegacy.protocols.release.protocol1_8to1_7_6_10.providers.GameProfileFetcher;
 
 public class AspirinLoader implements ViaPlatformLoader {
 	@Override
 	public void load() {
-		Via.getManager().getProviders().use(MovementTransmitterProvider.class, new VelocityMovementTransmitter());
 		Via.getManager().getProviders().use(VersionProvider.class, new AspirinVersionProvider());
 		Via.getManager().getProviders().use(CompressionProvider.class, new AspirinCompressionProvider());
 
