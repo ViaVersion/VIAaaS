9312450b69925c0f5d2799745db10f09857122cf
EnZaXD <60033407+FlorianMichael@users.noreply.github.com>
2023-11-10 16:03:38 +0100

    23w45a

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,8 +60,8 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
 
-    val vvVer = "4.9.0-23w44a-SNAPSHOT"
-    val vbVer = "4.9.0-23w43a-SNAPSHOT"
+    val vvVer = "4.9.0-23w45a-SNAPSHOT"
+    val vbVer = "4.9.0-23w45a-SNAPSHOT"
     val vrVer = "3.0.4-SNAPSHOT"
     implementation("com.viaversion:viaversion:$vvVer") { isTransitive = false }
     implementation("com.viaversion:viabackwards:$vbVer") { isTransitive = false }
