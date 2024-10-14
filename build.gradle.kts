0e79e971cafcb9b9efd3c454ba7044987422b6f2
RoblKyogre <roblkyogre@craftingcomrades.net>
2024-10-14 01:52:47 -0700

    update vv deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,14 +60,14 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
 
-    val vvVer = "5.0.2-SNAPSHOT"
-    val vbVer = "5.0.2-SNAPSHOT"
-    val vrVer = "4.0.2-SNAPSHOT"
+    val vvVer = "5.0.4-SNAPSHOT"
+    val vbVer = "5.0.4-SNAPSHOT"
+    val vrVer = "4.0.3-SNAPSHOT"
     implementation("com.viaversion:viaversion-common:$vvVer") { isTransitive = false }
     implementation("com.viaversion:viabackwards-common:$vbVer") { isTransitive = false }
     implementation("com.viaversion:viarewind-common:$vrVer") { isTransitive = false }
     implementation("net.raphimc:ViaAprilFools:3.0.1-SNAPSHOT")
-    implementation("net.raphimc:ViaLegacy:3.0.2-SNAPSHOT")
+    implementation("net.raphimc:ViaLegacy:3.0.3-SNAPSHOT")
 
     val nettyVer = "4.1.111.Final"
     implementation("io.netty:netty-handler-proxy:$nettyVer")
